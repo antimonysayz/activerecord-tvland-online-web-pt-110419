c88bb79415d6002fdad742214a227da4f7b93b2c
Anthony Mai <anthony.mai530@gmail.com>
2020-02-15 23:53:46 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,8 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
 
+  def actors_list
+    return self.characters.collect{|character| character.actor}
+  end
+
 end
